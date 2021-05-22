414c91a4d6c7d3575d34051665c0c7e77ad83b5b
Omar Moataz <omarmoataz@outlook.com>
2021-05-22 17:40:42 +0200

    Removed unnecessary report-form data testid.

--- app/javascript/components/project-submissions/components/flag-form.jsx
+++ app/javascript/components/project-submissions/components/flag-form.jsx
@@ -32,7 +32,7 @@ const FlagForm = ({ onSubmit, submission, userId }) => {
   }
 
   return (
-    <div data-testid="report-form">
+    <div>
       <h1 className="text-center accent">Flag Submission</h1>
 
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
